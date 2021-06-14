318ff37d48d45f7cc5240469fb53a687246591e0
Kai Zhu <kz2217@ic.ac.uk>
2021-06-14 12:56:31 +0800

    Uploaded to pypi

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as readme:
 # This call to setup() does all the work
 setup(
     name="xdrone",
-    version="0.2.8",
+    version="0.2.9",
     description="xDrone Language Server",
     long_description=long_description,
     long_description_content_type="text/markdown",
